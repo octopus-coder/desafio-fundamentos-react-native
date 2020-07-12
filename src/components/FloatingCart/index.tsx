83e8c0106fc14d670f5b10e0a72928e9b556dc24
Luis Enrique Gomez-Sanchez <luisen.goscas@gmail.com>
2020-07-12 15:26:21 -0300

    Desafio Pronto Fundamentos ReactNative

--- src/components/FloatingCart/index.tsx
+++ src/components/FloatingCart/index.tsx
@@ -24,15 +24,19 @@ const FloatingCart: React.FC = () => {
   const navigation = useNavigation();
 
   const cartTotal = useMemo(() => {
-    // TODO RETURN THE SUM OF THE PRICE FROM ALL ITEMS IN THE CART
-
-    return formatValue(0);
+    const totalPrice = products.reduce(
+      (accumulator, product) => accumulator + product.quantity * product.price,
+      0,
+    );
+    return formatValue(totalPrice);
   }, [products]);
 
   const totalItensInCart = useMemo(() => {
-    // TODO RETURN THE SUM OF THE QUANTITY OF THE PRODUCTS IN THE CART
-
-    return 0;
+    const totalQuantity = products.reduce(
+      (accumulator, product) => accumulator + product.quantity,
+      0,
+    );
+    return totalQuantity;
   }, [products]);
 
   return (
